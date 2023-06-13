8cc6c641379015c75a54ec964691d8781091b2c2
Tamutnih <renu07@list.ru>
2023-06-13 22:27:47 +0500

    empty project

---
+++ EmptyProject.java
@@ -0,0 +1,11 @@
+package main.java.com.example;
+
+public class EmptyProject {
+
+    // 1. Создать пустой проект в IntelliJ IDEA и прописать метод main();
+
+    public static void main(String[] args) {
+
+    }
+
+}
